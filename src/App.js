41a955f41a27a132d7f8168e96b54ee08aab8f9b
Vlad Serikov <vladikserikov@gmail.com>
2020-03-04 14:17:04 +0700

    start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+
+const App = () => {
+    
+
+    return (
+        <div>
+            textx
+        </div>
+    )
+}
+
+export default App;
